ea413d768c86a53b09d180fc6379a83c1c5b601b
alejandro3469 <alejandro.perez3469@gmail.com>
2024-05-14 21:25:04 -0600

    sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,57 @@
 import Image from "next/image";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main>
       <div className="top">
-        <div>Portfolio</div>
         <div>Alejandro Perez</div>
         <div>
           <ul className="social_media">
-            <li>Github</li>
-            <li>LinkedIn</li>
+            <li>
+              <FaGithub />
+            </li>
+            <li>
+              <FaLinkedin />
+            </li>
           </ul>
         </div>
       </div>
-      <div>
+      <section>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
-      </div>
-      <div>
-        <ul>
+      </section>
+      <img src="/gilles-lambert-pb_lF8VWaPU-unsplash.jpg" alt="" />
+      <section>
+        <ul className="skills">
           <li>Javascript</li>
           <li>Typescript</li>
           <li>React</li>
           <li>NextJS</li>
           <li>CSS</li>
         </ul>
-      </div>
-      <div className="projects_list">
-        <div className="project">
-          <div>Image</div>
-          <div>Lorem ipsum</div>
-          <div>Excepteur sint occaecat cupidatat non proident</div>
+      </section>
+      <section>
+        <h3>Latest projects</h3>
+        <div className="projects_list">
+          <div className="project">
+            <div>Image</div>
+            <div>Lorem ipsum</div>
+            <div>Excepteur sint occaecat cupidatat non proident</div>
+          </div>
+          <div className="project">
+            <div>Image</div>
+            <div>Lorem ipsum</div>
+            <div>Excepteur sint occaecat cupidatat non proident</div>
+          </div>
+          <div className="project">
+            <div>Image</div>
+            <div>Lorem ipsum</div>
+            <div>Excepteur sint occaecat cupidatat non proident</div>
+          </div>
         </div>
-        <div className="project">
-          <div>Image</div>
-          <div>Lorem ipsum</div>
-          <div>Excepteur sint occaecat cupidatat non proident</div>
-        </div>
-        <div className="project">
-          <div>Image</div>
-          <div>Lorem ipsum</div>
-          <div>Excepteur sint occaecat cupidatat non proident</div>
-        </div>
-      </div>
+      </section>
     </main>
   );
 }
