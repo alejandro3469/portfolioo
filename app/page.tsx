be96eefd8d9fff665aec6104532fa27fe72dcf65
alejandro3469 <alejandro.perez3469@gmail.com>
2024-05-19 22:21:44 -0600

    link icon

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { IoLogoJavascript } from "react-icons/io5";
 import { SiTypescript } from "react-icons/si";
 import { FaReact } from "react-icons/fa";
 import { SiNextdotjs } from "react-icons/si";
+import { VscLinkExternal } from "react-icons/vsc";
 import { FaCss3 } from "react-icons/fa";
 import { FaNodeJs } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
@@ -131,32 +132,39 @@ export default function Home() {
           <div className="projects_list">
             <Link href="https://food-menu-mu.vercel.app/la-casa-de-las-gargolas">
               <div className="project">
-                <img
-                  src="/albert-hu-RII9HuLDz4M-unsplash.jpg"
-                  alt=""
-                />
-                <div>Food menu</div>
-                <div className="description">{"Food app for restaurants with dashboard to add, delete and edit dishes and categories' names, descriptions, prices and availability"}</div>
+                <img src="/albert-hu-RII9HuLDz4M-unsplash.jpg" alt="" />
+                <div className="project_title">
+                  <span>Food menu</span>
+                  <VscLinkExternal />
+                </div>
+                <div className="description">
+                  {
+                    "Food app for restaurants with dashboard to add, delete and edit dishes and categories' names, descriptions, prices and availability"
+                  }
+                </div>
               </div>
             </Link>
             <Link href="https://event-chi-six.vercel.app/">
               <div className="project">
-                <img
-                  src="/nathan-dumlao-w5hhoYM_JsU-unsplash (1).jpg"
-                  alt=""
-                />
-                <div>Wedding website</div>
-                <div className="description">Website to display photos, wedding date, location and days left for couples</div>
+                <img src="/nathan-dumlao-w5hhoYM_JsU-unsplash (1).jpg" alt="" />
+                <div className="project_title">
+                  <span>Wedding website</span>
+                  <VscLinkExternal />
+                </div>
+                <div className="description">
+                  Website to display photos, wedding date, location and days
+                  left for couples
+                </div>
               </div>
             </Link>
 
             <Link href="https://rest-countries-docs.vercel.app/">
               <div className="project">
-                <img
-                  src="/vladislav-klapin-YeO44yVTl20-unsplash.jpg"
-                  alt=""
-                />
-                <div>Countries API</div>
+                <img src="/vladislav-klapin-YeO44yVTl20-unsplash.jpg" alt="" />
+                <div className="project_title">
+                  <span>Countries API</span>
+                  <VscLinkExternal />
+                </div>
                 <div className="description">REST API about countries data</div>
               </div>
             </Link>
